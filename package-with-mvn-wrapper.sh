21e923de30586e97cdf02e1182702dee9a9fd21e
WorkComputer <workcomputer@SMDKYAZs-MacBook-Pro.local>
2020-10-10 14:57:33 -0500

    added Dockerfiles for microservices

--- package-with-mvn-wrapper.sh
+++ package-with-mvn-wrapper.sh
@@ -1,2 +1,24 @@
-package-with-mvn-wrapper.sh
+# package-with-mvn-wrapper.sh
 ./mvnw clean package
+# git branch
+# git checkout dev
+# git branch feature/msp-4
+# git checkout feature/msp-4
+# git push --set-upstream origin feature/msp-4
+# git add .
+# git commit -m 'added packaging script'
+# git checkout dev
+# git merge feature/msp-4
+# git push origin dev
+# history
+# . ./package-with-mvn-wrapper.sh or bash package-with-mvn-wrapper.sh on ec2
+
+# All ports number
+# admin-server: 9090
+# api-gateway: 8080
+# config-server:8888
+# customer-service:8081
+# discovery-server:8761
+# hystrix-dashboard:7979
+# vets-service:8083
+# visits-service:8082
