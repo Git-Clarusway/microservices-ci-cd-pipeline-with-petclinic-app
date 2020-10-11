1ad76267d8d570bb2e270ec35c7af42c92cf76b9
WorkComputer <workcomputer@SMDKYAZs-MacBook-Pro.local>
2020-10-11 13:33:04 -0500

    added script for building docker images

--- build-dev-docker-images.sh
+++ build-dev-docker-images.sh
@@ -1,12 +1,12 @@
 # build-dev-docker-images.sh
 ./mvnw clean package
 dokerfile build --force-rm -t "petclinic-admin-sever:dev" ./spring-petclinic-admin-sever
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-api-getway
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-config-sever
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-costumer-sever
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-discover-sever
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-hystrix-dashboard
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-vets-service
-dokerfile build --force-rm -t "petclinic-api-getway-sever:dev" ./spring-petclinic-visits-service
+dokerfile build --force-rm -t "petclinic-api-getway:dev" ./spring-petclinic-api-getway
+dokerfile build --force-rm -t "petclinic-config-sever:dev" ./spring-petclinic-config-sever
+dokerfile build --force-rm -t "petclinic-costumer-sever:dev" ./spring-petclinic-costumer-sever
+dokerfile build --force-rm -t "petclinic-discover-sever:dev" ./spring-petclinic-discover-sever
+dokerfile build --force-rm -t "petclinic-hystrix-dashboard:dev" ./spring-petclinic-hystrix-dashboard
+dokerfile build --force-rm -t "petclinic-vets-service:dev" ./spring-petclinic-vets-service
+dokerfile build --force-rm -t "petclinic-visits-service:dev" ./spring-petclinic-visits-service
 dokerfile build --force-rm -t "petclinic-prometheus-sever:dev" ./docker/prometheus
 dokerfile build --force-rm -t "petclinic-grafana-sever:dev" ./docker/grafana
