a327bc9a4ac2c90f9d4b4cf5ac4b36d451cf45b1
WorkComputer <workcomputer@SMDKYAZs-MacBook-Pro.local>
2020-10-11 13:37:25 -0500

    added script for building docker images

--- build-dev-docker-images.sh
+++ build-dev-docker-images.sh
@@ -1,12 +1,12 @@
 # build-dev-docker-images.sh
 ./mvnw clean package
-dokerfile build --force-rm -t "petclinic-admin-sever:dev" ./spring-petclinic-admin-sever
-dokerfile build --force-rm -t "petclinic-api-getway:dev" ./spring-petclinic-api-getway
-dokerfile build --force-rm -t "petclinic-config-sever:dev" ./spring-petclinic-config-sever
-dokerfile build --force-rm -t "petclinic-costumer-sever:dev" ./spring-petclinic-costumer-sever
-dokerfile build --force-rm -t "petclinic-discover-sever:dev" ./spring-petclinic-discover-sever
-dokerfile build --force-rm -t "petclinic-hystrix-dashboard:dev" ./spring-petclinic-hystrix-dashboard
-dokerfile build --force-rm -t "petclinic-vets-service:dev" ./spring-petclinic-vets-service
-dokerfile build --force-rm -t "petclinic-visits-service:dev" ./spring-petclinic-visits-service
-dokerfile build --force-rm -t "petclinic-prometheus-sever:dev" ./docker/prometheus
-dokerfile build --force-rm -t "petclinic-grafana-sever:dev" ./docker/grafana
+docker build --force-rm -t "petclinic-admin-server:dev" ./spring-petclinic-admin-server
+docker build --force-rm -t "petclinic-api-gateway:dev" ./spring-petclinic-api-gateway
+docker build --force-rm -t "petclinic-config-server:dev" ./spring-petclinic-config-server
+docker build --force-rm -t "petclinic-customers-service:dev" ./spring-petclinic-customers-service
+docker build --force-rm -t "petclinic-discovery-server:dev" ./spring-petclinic-discovery-server
+docker build --force-rm -t "petclinic-hystrix-dashboard:dev" ./spring-petclinic-hystrix-dashboard
+docker build --force-rm -t "petclinic-vets-service:dev" ./spring-petclinic-vets-service
+docker build --force-rm -t "petclinic-visits-service:dev" ./spring-petclinic-visits-service
+docker build --force-rm -t "petclinic-prometheus-server:dev" ./docker/prometheus
+docker build --force-rm -t "petclinic-grafana-server:dev" ./docker/grafana
